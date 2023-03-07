8cfc9414cbf1dee738fe106776d58ba4bfd25229
yxshwxnt <yashwant.lalwani@gmail.com>
2023-03-08 04:05:05 +0530

    Modal Appearing

--- pages/components/About.jsx
+++ pages/components/About.jsx
@@ -1,12 +1,29 @@
-import Navbar from "./Navbar"
+import Navbar from "./Navbar"; 
+// import styles from '.../styles/About.Module.css';
 
 const About = () => {
-  return ( 
+  return (
     <>
-        <Navbar />
-        <div>About</div>
+      <Navbar />
+      {/* <div className={styles.aboutContainer}>  */}
+      <div className="conatiner-main">
+        <h1>About Us</h1>
+        <p>
+          We are a decentralized study resources website that aims to provide
+          students with access to high-quality educational materials.
+        </p>
+        <p>
+          Our platform is built on blockchain technology, which ensures that all
+          resources are securely stored and can be accessed from anywhere in the
+          world.
+        </p>
+        <p>
+          We believe that education is a fundamental right, and our mission is
+          to make it more accessible to everyone.
+        </p>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- pages/components/[Sem].jsx
+++ pages/components/[Sem].jsx
@@ -1,5 +1,5 @@
-import { Select } from "@chakra-ui/react";
 import {
+  Select,
   Modal,
   ModalOverlay,
   ModalContent,
@@ -7,14 +7,125 @@ import {
   ModalFooter,
   ModalBody,
   ModalCloseButton,
-  Lorem,
   Button,
+  Input,
 } from "@chakra-ui/react";
 import { useDisclosure } from "@chakra-ui/react";
+import { useState } from "react";
 import Navbar from "./Navbar";
 
 const Sem = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [selectedValues, setSelectedValues] = useState({});
+  const [isOpen, setIsOpen] = useState(false);
+  const [options, setOptions] = useState({
+    dropdown1: [
+      {
+        label: "DSA Notes",
+        value: "dsanotes",
+        pdfFile: '.../public/assets/java.pdf',
+      },
+      {
+        label: "DSA UT1 Papers",
+        value: "dsaut1",
+        pdfFile: './java.pdf',
+      },
+      {
+        label: "DSA UT2 Papers",
+        value: "dsaut2",
+        pdfFile: null,
+      },
+      {
+        label: "DSA ESE Papers",
+        value: "dsaese",
+        pdfFile: null,
+      },
+    ],
+    dropdown2: [
+      {
+        label: "SNS Notes",
+        value: "snsnotes",
+        pdfFile: null,
+      },
+      {
+        label: "SNS UT1 Papers",
+        value: "snsut1",
+        pdfFile: null,
+      },
+      {
+        label: "SNS UT2 Papers",
+        value: "snsut2",
+        pdfFile: null,
+      },
+      {
+        label: "SNS ESE Papers",
+        value: "snsese",
+        pdfFile: null,
+      },
+    ],
+    dropdown3: [
+      {
+        label: "Maths Notes",
+        value: "mathsnotes",
+        pdfFile: null,
+      },
+      {
+        label: "Maths UT1 Papers",
+        value: "mathsut1",
+        pdfFile: null,
+      },
+      {
+        label: "Maths UT2 Papers",
+        value: "mathsut2",
+        pdfFile: null,
+      },
+      {
+        label: "Maths ESE Papers",
+        value: "mathese",
+        pdfFile: null,
+      },
+    ],
+  });
+
+  const handleSelect = (event, dropdownName) => {
+    const newValue = event.target.value;
+    setSelectedValues((prevValues) => ({
+      ...prevValues,
+      [dropdownName]: newValue,
+    }));
+    setIsOpen(true);
+  };
+
+  const handleFileChange = (event, option, dropdownName) => {
+    const file = event.target.files[0];
+    const updatedOptions = {
+      ...options,
+      [dropdownName]: options[dropdownName].map((o) =>
+        o.value === option.value ? { ...o, pdfFile: file } : o
+      ),
+    };
+    setOptions(updatedOptions);
+  };
+
+  const handleDownload = (pdfFile) => {
+    // const url = URL.createObjectURL(pdfFile);
+    // const link = document.createElement("a");
+    // link.href =  '.../public/assets/java.pdf'; 
+    // console.log(link.href); 
+    // link.download = pdfFile.name;
+    // document.body.appendChild(link);
+    // link.click();  
+
+    var link = document.createElement('a');
+    link.href = pdfFile;
+    link.download = 'file.pdf';
+    link.dispatchEvent(new MouseEvent('click'));
+  };
+
+  const handleCloseModal = () => {
+    setSelectedValues({});
+    setIsOpen(false);
+  };
+
   return (
     <>
       <Navbar />
@@ -28,73 +139,98 @@ const Sem = () => {
         <div className="container-main">
           <h1>Subject Wise Resources</h1>
           <hr />
-          <div className="item">
+          <div className="item"> 
+
+            {/* <Select
+              value={selectedValues.dropdown2}
+              onChange={(e) => handleSelect(e, "dropdown2")}
+            >
+              {options.dropdown2.map((option) => (
+                <option key={option.value} value={option.value}>
+                  {option.label}
+                </option>
+              ))}
+            </Select> */}
+
             <h3>DSA</h3>
-            <Select placeholder="Select option">
-              <option value="option1" onSelect={onOpen}>
-                Notes
-              </option>
-              <option value="option2" onSelect={onOpen}>
-                UT1 Papers
-              </option>
-              <option value="option3" onSelect={onOpen}>
-                UT2 Papers
-              </option>
-              <option value="option4" onSelect={onOpen}>
-                End Sem Paper
-              </option>
+            <Select 
+              placeholder="Select option"
+              value={selectedValues.dropdown1}
+              onChange={(e) => handleSelect(e, "dropdown1")}
+            >
+              {options.dropdown1.map((option) => (
+                <option key={option.value} value={option.value}>
+                  {option.label}
+                </option>
+              ))}
             </Select>
           </div>
           <div className="item">
             <h3>SNS</h3>
-            <Select placeholder="Select option">
-              <option value="option1" onSelect={onOpen}>
-                Notes
-              </option>
-              <option value="option2" onSelect={onOpen}>
-                UT1 Papers
-              </option>
-              <option value="option3" onSelect={onOpen}>
-                UT2 Papers
-              </option>
-              <option value="option4" onSelect={onOpen}>
-                End Sem Paper
-              </option>
+            <Select
+              placeholder="Select option"
+              value={selectedValues.dropdown2}
+              onChange={(e) => handleSelect(e, "dropdown2")}
+            >
+              {options.dropdown2.map((option) => (
+                <option key={option.value} value={option.value}>
+                  {option.label}
+                </option>
+              ))}
             </Select>
           </div>
           <div className="item">
             <h3>Maths</h3>
-            <Select placeholder="Select option">
-              <option value="option1" onSelect={onOpen}>
-                Notes
-              </option>
-              <option value="option2" onSelect={onOpen}>
-                UT1 Papers
-              </option>
-              <option value="option3" onSelect={onOpen}>
-                UT2 Papers
-              </option>
-              <option value="option4" onSelect={onOpen}>
-                End Sem Paper
-              </option>
+            <Select
+              placeholder="Select option"
+              value={selectedValues.dropdown3}
+              onChange={(e) => handleSelect(e, "dropdown3")}
+            >
+          {options.dropdown3.map((option) => (
+                <option key={option.value} value={option.value}>
+                  {option.label}
+                </option>
+              ))}
             </Select>
           </div>
         </div>
       </div>
-      <Modal isOpen={isOpen} onClose={onClose}>
+
+      <Modal isOpen={isOpen} onClose={handleCloseModal}>
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>Modal Title</ModalHeader>
+          <ModalHeader>
+            Selected Values: {JSON.stringify(selectedValues)}
+          </ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-            <Lorem count={2} />
+            {Object.entries(options).map(([dropdownName, dropdownOptions]) =>
+              dropdownOptions.map((option) =>
+                option.value === selectedValues[dropdownName] ? (
+                  <div key={option.value}>
+                    <Input
+                      type="file"
+                      onChange={(event) =>
+                        handleFileChange(event, option, dropdownName)
+                      }
+                    />
+                    {option.pdfFile && (
+                      <Button
+                        onClick={() => handleDownload(option.pdfFile)}
+                        mt={4}
+                      >
+                        Download PDF
+                      </Button>
+                    )}
+                  </div>
+                ) : null
+              )
+            )}
           </ModalBody>
-
           <ModalFooter>
-            <Button colorScheme="blue" mr={3} onClick={onClose}>
+            <Button colorScheme="blue" onClick={handleCloseModal}>
               Close
             </Button>
-            <Button variant="ghost">Download</Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
