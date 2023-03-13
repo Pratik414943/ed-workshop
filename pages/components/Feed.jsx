5c041de3854da5dbffe33cdd71e333a9a755293f
Yashwant Lalwani <yashwant.lalwani@gmail.com>
2023-03-13 21:11:48 +0530

    Map for Feed

--- pages/components/Feed.jsx
+++ pages/components/Feed.jsx
@@ -1,17 +1,38 @@
 import Navbar from "./Navbar"; 
 import TweetBox from "./TweetBox"; 
-import Post from "./Post"; 
+import Post from "./Post";  
+import { useState } from "react";
 
 const Feed = () => {
-  
+  const [posts, setPosts] = useState([
+    {
+      id: 1,
+      author: 'John Doe',
+      content: 'I love football!',
+    },
+    {
+      id: 2,
+      author: 'Jane Smith',
+      content: 'Who do you think will win the World Cup?',
+    }, 
+    {
+      id: 3, 
+      author: 'Yashwant', 
+      content: 'Suiiiiiiii'
+    }
+  ]);  
   return (
     <>
       <Navbar />
       <div className="main-feed">
         <TweetBox /> 
-        <Post />
-        <Post />
-        <Post />
+        {posts.map((post) => (
+        // <div key={post.id}>
+        //   <h2>{post.author}</h2>
+        //   <p>{post.content}</p>
+        // </div> 
+        <Post author={post.author} content={post.content} /> 
+      ))}
       </div>
     </>
   );
--- pages/components/Post.jsx
+++ pages/components/Post.jsx
@@ -1,19 +1,35 @@
-const Post = () => {
+import { color } from "framer-motion";
+import { createAvatar } from '@dicebear/core';
+import { lorelei } from '@dicebear/collection';
+
+const Post = (props) => {
+  const avatar = createAvatar(lorelei, {
+    seed: 'John Doe',
+  });
+  const svg = avatar.toString(); 
+  // console.log(svg);
   return (
     <>
       <div className="wrapper">
-        <div className="text">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates
-          quaerat sint earum, distinctio, dolor eveniet atque eaque voluptatibus
-          vitae illo fugiat rerum unde quas explicabo.
+        <div className="author">
+          <span>{props.author}</span>
         </div>
+        <div className="text">{props.content}</div>
         <div className="bottom">
-            <ul className="icons">
-                <li><i class="fa-regular fa-comment"></i></li>
-                <li><i class="fa-solid fa-arrow-down"></i></li>
-                <li><i class="fa-solid fa-arrow-up"></i></li>
-                <li><i class="fa-solid fa-share"></i></li>
-            </ul>
+          <ul className="icons">
+            <li>
+              <i class="fa-regular fa-comment"></i>
+            </li>
+            <li>
+              <i class="fa-solid fa-arrow-down"></i>
+            </li>
+            <li>
+              <i class="fa-solid fa-arrow-up"></i>
+            </li>
+            <li>
+              <i class="fa-solid fa-share"></i>
+            </li>
+          </ul>
         </div>
       </div>
     </>
